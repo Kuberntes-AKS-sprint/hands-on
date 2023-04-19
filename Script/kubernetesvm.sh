87cb90a2a9d7647b0910495bc68d973eac63dd4c
dodream28 <91585060+dodream28@users.noreply.github.com>
2023-04-19 14:54:40 +0900

    Update kubernetesvm.sh

--- Script/kubernetesvm.sh
+++ Script/kubernetesvm.sh
@@ -30,6 +30,19 @@ az vm create \
     --admin-password Azuresprint12#$ \
     --public-ip-sku basic \
     --custom-data cloud-init.txt
+    
+az vm create \
+    --resource-group sprint-RG \
+    --name sprint-worker-02 \
+    --size Standard_B1s \
+    --image UbuntuLTS \
+    --location koreacentral \
+    --storage-sku Standard_LRS \
+    --authentication-type password \
+    --admin-username sprint \
+    --admin-password Azuresprint12#$ \
+    --public-ip-sku basic \
+    --custom-data cloud-init.txt
 
 ## 방화벽 설정 sprint-master-01
 az vm open-port --resource-group sprint-RG --name sprint-master-01 --port 80 --priority 310
@@ -42,5 +55,11 @@ az vm open-port --resource-group sprint-RG --name sprint-worker-01 --port 80 --p
 az vm open-port --resource-group sprint-RG --name sprint-worker-01 --port 5000 --priority 320
 az vm open-port --resource-group sprint-RG --name sprint-worker-01 --port 2049 --priority 330
 az vm open-port --resource-group sprint-RG --name sprint-worker-01 --port 443 --priority 340
+
+## 방화벽 설정 sprint-worker-02
+az vm open-port --resource-group sprint-RG --name sprint-worker-02 --port 80 --priority 310
+az vm open-port --resource-group sprint-RG --name sprint-worker-02 --port 5000 --priority 320
+az vm open-port --resource-group sprint-RG --name sprint-worker-02 --port 2049 --priority 330
+az vm open-port --resource-group sprint-RG --name sprint-worker-02 --port 443 --priority 340
 
 
