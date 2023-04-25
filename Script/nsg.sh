f9054217a36a2a5969ebca15d96b1e5803e72d8c
dodream28 <91585060+dodream28@users.noreply.github.com>
2023-04-25 13:42:47 +0900

    Create nsg.sh

---
+++ Script/nsg.sh
@@ -0,0 +1,4 @@
+#!bin/bash
+
+az vm open-port --resource-group sprint-RG --name sprint-worker-01 --port {} --priority 350
+az vm open-port --resource-group sprint-RG --name sprint-worker-02 --port {} --priority 350
