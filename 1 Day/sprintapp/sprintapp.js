fa9de6f24c6acbad7805036d8a315e6716cb16a0
root <root@sprint-master-01.3idav10k04nuvpyegkr5oqcwld.syx.internal.cloudapp.net>
2023-10-04 05:43:08 +0000

    sprint:231004

--- 1 Day/sprintapp/sprintapp.js
+++ 1 Day/sprintapp/sprintapp.js
@@ -6,7 +6,7 @@ console.log("Sprint Server starting...");
 var handler = function(request, response) {
     console.log("Received Request from" + request.connection.remoteAddress);
     response.writeHead(200);
-    response.end("스프린트 Pod " + os.hostname() + "생성 되었습니다."+"\n");
+    response.end("새로운" + os.hostname() + "생성 되었습니다."+"\n");
 };
 
 var www = http.createServer(handler);
